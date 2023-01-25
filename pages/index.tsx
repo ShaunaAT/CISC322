79f821d6f48d7e78643ba0e78f33daff420c1bae
Shauna Tuinstra <shaunaat@gmail.com>
2023-01-24 20:38:54 -0500

    update site again

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import bitcoin from '../public/bitcoin.avif'
+import bitcoin from '../public/bitcoin1.jpg'
 import blockchain from '../public/blockchain.jpg'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,36 +15,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main className={styles.main}>
         <h1 className={styles.landingText}>
-          CISC 322 Group 39
+          CISC 322 - Group 39
         </h1>
+        <h2 className={styles.landingText}>Mercy Doan, Sam Lownie, Shauna Tuinstra, Cain Susko, Alice Slabosz, Yash Patel</h2>
         <div>
           <Image
-            src={blockchain}
+            src={bitcoin}
             alt='Bitcoin'
             fill
             className={styles.landingImage}
             />
         </div>
-        <div className="row">
-          <div className="col-sm-6">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Deliverable</h5>
-                <p className="card-text">Deliverable deets</p>
-                <a href="#" className="btn btn-primary">View</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Deliverable</h5>
-                <p className="card-text">Deliverable deets</p>
-                <a href="#" className="btn btn-primary">View</a>
-              </div>
-            </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Deliverable</h5>
+            <p className="card-text">Deliverable details, introduction, etc.</p>
+            <a href="#" className="btn btn-primary">View</a>
           </div>
         </div>
       </main>
